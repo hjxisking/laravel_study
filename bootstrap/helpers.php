da90396176713e0d0f282e4b3800c5aef223c37d
胡建新 <>
2020-02-06 13:36:34 +0800

    基本的首页

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,4 +1,12 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
 function test_helper() {
     echo "ok";
+}
+
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
 }
