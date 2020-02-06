d9736288776133d0eeb3d13a98e66315cd39b9a8
胡建新 <>
2020-02-06 19:16:18 +0800

    用户界面 - 商品详情页

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,4 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
