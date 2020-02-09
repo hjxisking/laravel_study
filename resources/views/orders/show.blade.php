adfa7287db31dc4409258fe6a819f4cb60ac11fd
胡建新 <>
2020-02-09 16:23:42 +0800

    订单模块调整 没有支付逻辑以及支付的事件

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -27,9 +27,7 @@
                                         </a>
                                     </div>
                                     <div>
-              <span class="product-title">
-                 <a target="_blank" href="{{ route('products.show', [$item->product_id]) }}">{{ $item->product->title }}</a>
-              </span>
+                                        <span class="product-title"><a target="_blank" href="{{ route('products.show', [$item->product_id]) }}">{{ $item->product->title }}</a></span>
                                         <span class="sku-title">{{ $item->productSku->title }}</span>
                                     </div>
                                 </td>
@@ -103,8 +101,10 @@
                                             <button type="button" id="btn-receive" class="btn btn-sm btn-success">确认收货</button>
                                         </div>
                                     @endif
-                                    <!-- 订单已支付，且退款状态是未退款时展示申请退款按钮 -->
-                                    @if($order->paid_at && $order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)
+                                    <!-- 不是众筹订单，已支付，且退款状态是未退款时展示申请退款按钮 -->
+                                    @if($order->type !== \App\Models\Order::TYPE_CROWDFUNDING &&
+                                        $order->paid_at &&
+                                        $order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)
                                         <div class="refund-button">
                                             <button class="btn btn-sm btn-danger" id="btn-apply-refund">申请退款</button>
                                         </div>
